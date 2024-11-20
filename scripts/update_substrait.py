44ed0372cc67db14b7695614cbf4a66b303ba13c
Chandra Sanapala <chandra@sundeck.io>
2024-11-20 16:06:53 +0530

    chore: update substrait proto

--- scripts/update_substrait.py
+++ scripts/update_substrait.py
@@ -5,7 +5,7 @@ import os
 import shutil
 from os import walk
 
-GITHUB_TAG = "a0a10bb39dae32a5564f4f37a4673068a08c8e91" # v0.59.0
+GITHUB_TAG = "a64a1025280d89bddb8983438ce9dda99ac4b05e" # v0.61.0
 # Change to substrait folder
 sub_folder  = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','third_party','substrait')
 os.chdir(sub_folder)
