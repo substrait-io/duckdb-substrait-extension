f55d3555e14c8f596cae40d73c97a6984368ad32
Chandra Sanapala <chandra@sundeck.io>
2024-12-18 10:09:27 +0530

    add test for 1 pt + 1 transform and column reduction

--- test/c/test_projection.cpp
+++ test/c/test_projection.cpp
@@ -41,15 +41,32 @@ TEST_CASE("Test C Project 1 input column 1 transformation with Substrait API", "
 	REQUIRE(CHECK_COLUMN(result, 1, {100, 400, 900}));
 }
 
-TEST_CASE("Test C Project two columns with Substrait API", "[substrait-api]") {
+TEST_CASE("Test C Project two passthrough columns with Substrait API", "[substrait-api]") {
 	DuckDB db(nullptr);
 	Connection con(db);
 
 	CreateEmployeeTable(con);
 
-	// TODO should this have any projection?
+	// This should not have a ProjectRel node
 	auto json_str = con.GetSubstraitJSON("SELECT name, salary FROM employees");
+	auto expected_json_str = R"({"relations":[{"root":{"input":{"read":{"baseSchema":{"names":["employee_id","name","department_id","salary"],"struct":{"types":[{"i32":{"nullability":"NULLABILITY_REQUIRED"}},{"string":{"nullability":"NULLABILITY_NULLABLE"}},{"i32":{"nullability":"NULLABILITY_NULLABLE"}},{"decimal":{"scale":2,"precision":10,"nullability":"NULLABILITY_NULLABLE"}}],"nullability":"NULLABILITY_REQUIRED"}},"projection":{"select":{"structItems":[{"field":1},{"field":3}]},"maintainSingularStruct":true},"namedTable":{"names":["employees"]}}},"names":["name","salary"]}}],"version":{"minorNumber":53,"producer":"DuckDB"}})";
+	REQUIRE(json_str == expected_json_str);
+	auto result = con.FromSubstraitJSON(json_str);
+	REQUIRE(CHECK_COLUMN(result, 0, {"John Doe", "Jane Smith", "Alice Johnson", "Bob Brown", "Charlie Black"}));
+	REQUIRE(CHECK_COLUMN(result, 1, {120000, 80000, 50000, 95000, 60000}));
+}
+
+TEST_CASE("Test C Project one passthrough column one transformation with Substrait API", "[substrait-api]") {
+	DuckDB db(nullptr);
+	Connection con(db);
+
+	CreateEmployeeTable(con);
+
+	// TODO make this work
+	auto json_str = con.GetSubstraitJSON("SELECT name, salary * 1.2 as new_salary FROM employees");
+	// auto expected_json_str = R"()";
 	// REQUIRE(json_str == expected_json_str);
 	auto result = con.FromSubstraitJSON(json_str);
 	REQUIRE(CHECK_COLUMN(result, 0, {"John Doe", "Jane Smith", "Alice Johnson", "Bob Brown", "Charlie Black"}));
+	REQUIRE(CHECK_COLUMN(result, 1, {144000, 96000, 60000, 114000, 72000}));
 }
