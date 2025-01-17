1c3323a36153c59c0d170183938b3090ffc89769
Chandra Sanapala <chandra@sundeck.io>
2025-01-17 07:59:41 +0530

    Fix build error

--- src/to_substrait.cpp
+++ src/to_substrait.cpp
@@ -1644,7 +1644,7 @@ substrait::Rel *DuckDBToSubstrait::TransformDeleteTable(LogicalOperator &dop) {
 	return rel;
 }
 
-vector<LogicalType>::__alloc_traits::size_type DuckDBToSubstrait::GetColumnCount(LogicalOperator &dop) {
+vector<LogicalType>::size_type DuckDBToSubstrait::GetColumnCount(LogicalOperator &dop) {
 	return dop.types.size();
 }
 
